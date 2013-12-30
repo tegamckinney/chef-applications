86af5ff6ea7dc89044137152388eda2a3f424528
Tega McKinney <tega.mckinney@freshedgesolutions.com>
2013-12-29 21:54:38 -0500

    Changing Charles proxy to use Cask

--- recipes/charlesproxy.rb
+++ recipes/charlesproxy.rb
@@ -1,3 +1,2 @@
-applications_package "Charles" do
-  source  "http://c720206.r6.cf2.rackcdn.com/charles-proxy_3.6.5.zip"
-end
+include_recipe "applications::homebrewcask"
+applications_cask "charles"
